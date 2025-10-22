d2d78ef875c2e4a14b6b26b22936a4239302e89e
joselinena123-collab <joselinena123@gmail.com>
2025-10-22 09:45:48 -0600

    Add files via upload

---
+++ Practica2/error.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Error de Login</title>
+</head>
+<body>
+    <h1>ERROR</h1>
+    <h2>Descripción</h2>
+    <p>Las informacion  ingresada no es correcta. Por favor, intenta nuevamente.</p>
+    
+    <br>
+    <a href="login.php">REGRESAR</a>
+</body>
+</html>
